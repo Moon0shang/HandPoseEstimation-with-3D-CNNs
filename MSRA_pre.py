850dfe6989f860baf451bc059fbfec4ce7391035
X <moon0shang@gmail.com>
2018-10-17 22:45:40 +0800

    a

--- MSRA_pre.py
+++ MSRA_pre.py
@@ -17,11 +17,14 @@ class Read_MSRA(object):
 
     def __init__(self):
         self.MSRA_valid = self.read_valid()
+
         # self.read_all()
 
     def read_valid(self):
+
         raw = sio.loadmat('./msra_valid.mat')
         data = raw['msra_valid']
+
         return data
 
     def read_all(self):
@@ -41,14 +44,14 @@ class Read_MSRA(object):
                 ges_dir = os.path.join(
                     dataset_dir, subject_names[sub], gesture_names[ges])
 
-                save_ges_dir = os.path.join(
+                self.save_ges_dir = os.path.join(
                     save_dir, subject_names[sub], gesture_names[ges])
 
                 # read ground truth
-                ground_truth = self.read_ground_truth(ges_dir, save_ges_dir)
+                [ground_truth, frame_num] = self.read_ground_truth(ges_dir)
                 # create save files
                 try:
-                    os.mkdir(save_ges_dir)
+                    os.mkdir(self.save_ges_dir)
                 except:
                     print('2nd directory already exist!')
                 else:
@@ -56,10 +59,12 @@ class Read_MSRA(object):
                           subject_names[sub]+'/'+gesture_names[ges])
 
                 [jnt_xyz, hand_points] = self.read_depth_bin(
-                    ges_dir, save_ges_dir, sub, ges, ground_truth)
-                self.save_mat(save_ges_dir, jnt_xyz, hand_points)
+                    ges_dir, frame_num, sub, ges, ground_truth)
+                self.save_mat(jnt_xyz, hand_points)
 
-    def read_ground_truth(self, ges_dir, save_ges_dir):
+        return hand_points
+
+    def read_ground_truth(self, ges_dir):
 
         joint_txt = os.path.join(ges_dir, 'joint.txt')
 
@@ -71,26 +76,30 @@ class Read_MSRA(object):
         # 因为深度图z是正的，但是label里面z却是负数
         ground_truth[:, :, 2] = -ground_truth[:, :, 2]
 
-        return ground_truth
+        return ground_truth, frame_num
 
-    def read_depth_bin(self, ges_dir, save_ges_dir, sub, ges, ground_truth):
+    def read_depth_bin(self, ges_dir, frame_num, sub, ges, ground_truth):
 
         num = 0
+        sample_num = 1024
         for i in os.listdir(ges_dir):
             if os.path.splitext(i)[1] == '.bin':
                 num += 1
 
         valid = self.MSRA_valid[sub, ges]
+        # hand_points_3d = np.zeros((frame_num, sample_num, 3))
         for frm in range(num):
             if not valid[frm]:
                 continue
-            hand_points = self.read_conv_bin(ges_dir, frm, save_ges_dir)
+            hand_points = self.read_conv_bin(ges_dir, frm)
             jnt_xyz = np.squeeze(ground_truth[frm, :, :])
 
-        return jnt_xyz, hand_points
+            # hand_points_3d[frm, :, :] = hand_points
+
+        return jnt_xyz, hand_points  # hand_points_3d
 
         # read binary files
-    def read_conv_bin(self, ges_dir, frm, save_ges_dir):
+    def read_conv_bin(self, ges_dir, frm):
 
         with open(ges_dir + '/' + str('%06d' % frm) + '_depth.bin', 'rb') as f:
             img_width = struct.unpack('I', f.read(4))[0]
@@ -111,7 +120,7 @@ class Read_MSRA(object):
         hand_depth = hand_depth.reshape(bb_width, bb_height)
         hand_depth = hand_depth.transpose()
 
-        np.savetxt(save_ges_dir+'/depth.txt', hand_depth)
+        np.savetxt(self.save_ges_dir + '/depth.txt', hand_depth)
 
         fFocal_msra = 241.42
         hand_3d = np.zeros((valid_pixel_num, 3))
@@ -123,12 +132,12 @@ class Read_MSRA(object):
                 0 < idx < h * w -1
                 '''
                 idx = jj * bb_height + ii
-                hand_3d[idx, 0] = -(img_width/2 - (jj + bb_left)
-                                    ) * hand_depth[ii, jj]/fFocal_msra
-                hand_3d[idx, 1] = -(img_height/2 - (ii + bb_top)
-                                    ) * hand_depth[ii, jj] / fFocal_msra
+                hand_3d[idx, 0] = -(img_width/2 - jj - bb_left) * \
+                    hand_depth[ii, jj] / fFocal_msra
+                hand_3d[idx, 1] = -(img_height/2 - ii - bb_top) * \
+                    hand_depth[ii, jj] / fFocal_msra
                 hand_3d[idx, 2] = hand_depth[ii, jj]
-        np.savetxt(save_ges_dir+'/depth_3d.txt', hand_3d)
+        np.savetxt(self.save_ges_dir+'/depth_3d.txt', hand_3d)
 
         valid_idx = []
 
@@ -138,14 +147,14 @@ class Read_MSRA(object):
 
         hand_points = hand_3d[valid_idx, :]
 
-        np.savetxt(save_ges_dir+'/v_3D.txt', hand_points)
+        np.savetxt(self.save_ges_dir + '/v_3D.txt', hand_points)
 
         return hand_points
 
-    def save_mat(self, save_ges_dir, jnt_xyz, hand_points):
+    def save_mat(self,  jnt_xyz, hand_points):
 
-        sio.savemat(save_ges_dir+'/jnt_xyz.mat', {'joint': jnt_xyz})
-        sio.savemat(save_ges_dir+'/hand_points.mat',
+        sio.savemat(self.save_ges_dir+'/jnt_xyz.mat', {'joint': jnt_xyz})
+        sio.savemat(self.save_ges_dir+'/hand_points.mat',
                     {'hand_points': hand_points})
 
 
