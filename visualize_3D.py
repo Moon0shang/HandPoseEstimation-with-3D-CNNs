b7d3977b075e883ef3a55ee0ffba95ae30832623
X <moon0shang@gmail.com>
2018-10-18 11:19:00 +0800

    add 3d matrix support

--- visualize_3D.py
+++ visualize_3D.py
@@ -1,27 +1,29 @@
-import os
-import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-hand_points_raw = sio.loadmat('./results/P0/1/hand_points.mat')
-hand_points = hand_points_raw['hand_points']
-
 
 def visualize(hand_points):
+
+    if len(hand_points.shape) == 2:
+        x = hand_points[:, 0]
+        y = hand_points[:, 1]
+        z = hand_points[:, 2]
+    elif len(hand_points.shape) == 3:
+        x = None
+        y = None
+        z = None
+    else:
+        print('hand_points is not the right matrix!')
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(
-        hand_points[:, 0],
-        hand_points[:, 1],
-        hand_points[:, 2]
-
+        x, y, z,
+        marker='.'  # show as a point
     )
     ax.axis('scaled')
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
     plt.show()
-
-
-show_3d = visualize(hand_points)
