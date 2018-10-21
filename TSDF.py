e7555107e0569ca613b26e725a18260b9c0ac63f
X <moon0shang@gmail.com>
2018-10-21 22:58:32 +0800

    done

--- TSDF.py
+++ TSDF.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import scipy.io as sio
 
 """ 
 def set_m(resolution=32):
@@ -8,45 +8,78 @@ def set_m(resolution=32):
  """
 
 
-def tsdf(hand_points, depth_ori, pic_info):
+def tsdf(hand_points, hand_ori, pic_info):
 
     voxel_res = 32
+    """ 
     [fFocal_msra, img_height, img_width, bb_top, bb_bottom,
-        bb_left, bb_right, bb_width, bb_height] = pic_info
-    blockdim = (depth_ori.size+128-1)/128
-    point_max_min = max_min_point(blockdim, hand_points, pic_info)
+        bb_left, bb_right, bb_width, bb_height] = pic_info """
+    # blockdim = (hand_ori.size+128-1)//128
+    depth_ori = hand_ori[:, 2]
+    point_max, point_min = max_min_point(hand_ori)
 
     # 不知道用途，也许可以删除
     # if any(point_max_min):
     # point_max_min = point_max_min[:-1] """
 
-    point_max = np.max(point_max_min[:, 3:6], axis=0)
-    point_min = np.min(point_max_min[:, :3], axis=0)
+    # point_max = np.max(point_max_min[:, 3:6], axis=0)
+    # point_min = np.min(point_max_min[:, :3], axis=0)
+
     point_mid = (point_max + point_min) / 2
     len_pixel = point_max - point_min
     max_lenth = np.max(len_pixel)
     voxel_len = max_lenth / voxel_res
     truncation = voxel_len * 3
     vox_ori = point_mid - max_lenth / 2 + voxel_len / 2
-
     # tsdf calculation
-    tsdf = tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation)
 
+    tsdf_v = tsdf_cal(depth_ori, pic_info, vox_ori,
+                      voxel_len, truncation, point_mid)
+
+    sio.savemat('./tsdf/tsdf.mat', {'tsdf': tsdf_v})
+
+    return tsdf_v
 
 # need to be rewrite
-def max_min_point(blockdim, depth_ori, pic_info):
+
+
+def max_min_point(hand_ori):
+
+    # trying
+    x = hand_ori[:, 0]
+    y = hand_ori[:, 1]
+    z = hand_ori[:, 2]
+    # 防止出现max(z)= 0 的情况
+    z = z[z != 0]
+
+    point_max = np.empty([3], dtype=np.float32)
+    point_min = np.empty([3], dtype=np.float32)
+
+    point_max[0] = np.max(x)
+    point_max[1] = np.max(y)
+    point_max[2] = np.max(z)
+    point_min[0] = np.min(x)
+    point_min[1] = np.min(y)
+    point_min[2] = np.min(z)
+
+    return point_max, point_min
+
+
+""" 
+wrong method that takes too much time, don't use it!
+def max_min_point(blockdim, hand_ori, pic_info):
 
     [fFocal_msra, img_height, img_width, bb_top, bb_bottom,
         bb_left, bb_right, bb_width, bb_height] = pic_info
 
-    mm_threadperblock = 128
+    # mm_threadperblock = 128
     center_x = 160
     center_y = 120
     point_max_min = np.empty([blockdim, 6], dtype=np.float32)
 
     point_min = np.empty((128, 3), dtype=np.float32)
     point_max = np.empty((128, 3), dtype=np.float32)
-    '''此处需要一个循环'''
+    # '''此处需要一个循环'''
     for bid in range(blockdim):
         for tid in range(128):
             # cuda.grid = threadidx.x+blockidx.x*blockdim.x
@@ -55,7 +88,7 @@ def max_min_point(blockdim, depth_ori, pic_info):
             pos = tid+bid*128
             x = pos % bb_width + bb_left
             y = pos / bb_width + bb_top
-            cam_z = depth_ori[pos]
+            cam_z = hand_ori[pos]
             if abs(cam_z) < 1:
                 point_min[tid][0], point_min[tid][1], point_min[tid][2] = 9999, 9999, 9999
                 point_max[tid][0], point_max[tid][1], point_max[tid][2] = - \
@@ -84,7 +117,8 @@ def max_min_point(blockdim, depth_ori, pic_info):
                     point_max[index][2] = min(
                         point_max[index+s][2], point_max[index][2])
 
-                    s *= 2
+                s *= 2
+
             if tid == 0:
                 point_max_min[bid][0] = point_min[0][0]
                 point_max_min[bid][1] = point_min[0][1]
@@ -93,7 +127,9 @@ def max_min_point(blockdim, depth_ori, pic_info):
                 point_max_min[bid][4] = point_max[0][1]
                 point_max_min[bid][5] = point_max[0][2]
 
+    return point_max_min
 
+ """
 """ 
     x = hand_points[:, 0]
     y = hand_points[:, 1]
@@ -111,7 +147,7 @@ def max_min_point(blockdim, depth_ori, pic_info):
  """
 
 
-def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation):
+def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation, point_mid):
 
     [fFocal_msra, img_height, img_width, bb_top, bb_bottom,
         bb_left, bb_right, bb_width, bb_height] = pic_info
@@ -120,13 +156,17 @@ def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation):
     y_center = 120
     volume_size = 32 * 32 * 32
 
-    for z in range(32):
+    tsdf_v = np.empty([3, 32, 32, 32])
+    # sdf = np.empty([32, 32, 32])
+    for x in range(32):
         for y in range(32):
-            for x in range(32):
+            for z in range(32):
+
                 voxel_idx = x + y * 32 + z * 32 * 32
                 if voxel_idx >= volume_size:
-                    print('out of range')
-                    return
+                    # print('out of range')
+                    continue
+
                 # voxel center
                 voxel_x = vox_ori[0] + x * voxel_len
                 voxel_y = vox_ori[1] + y * voxel_len
@@ -135,22 +175,27 @@ def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation):
                 # voxel center in image frame
                 coeff = -fFocal_msra / voxel_z
                 pixel_x = int((voxel_x * coeff + x_center))
-                pixel_y = int((voxel_y * coeff + y_center))
-                # need to modify
-                tsdf_v = np.zeros(3, z, y, x)  # not 100
+                pixel_y = int((-voxel_y * coeff + y_center))
+                '''need to modify'''
+
                 if pixel_x < bb_left or pixel_x >= bb_right or pixel_y < bb_top or pixel_y >= bb_bottom:
-                    print('out of valid area')
-                    return
-                idx = (pixel_y - bb_top) * bb_width + pixel_x - bb_left
+                    # print('out of valid area')
+                    continue
+
+                idx = int((pixel_y - bb_top) * bb_width + pixel_x - bb_left)
                 pixel_depth = depth_ori[idx]
+
                 if abs(pixel_depth) < 1:
-                    print('wrong depth')
-                    return
+                    # print('wrong depth')
+                    continue
+
                 # closest surface point in world frame
+
                 coeff1 = pixel_depth / fFocal_msra
                 world_x = (pixel_x - x_center) * coeff1
                 world_y = (pixel_y - y_center) * coeff1
                 world_z = -pixel_depth
+
                 tsdf_x = abs(voxel_x - world_x) / truncation
                 tsdf_y = abs(voxel_y - world_y) / truncation
                 tsdf_z = abs(voxel_z - world_z) / truncation
@@ -174,7 +219,7 @@ def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation):
                 tsdf_v[1, z, y, x] = tsdf_y
                 tsdf_v[2, z, y, x] = tsdf_z
 
-                return tsdf_v
+    return tsdf_v
 
 
 """ 
