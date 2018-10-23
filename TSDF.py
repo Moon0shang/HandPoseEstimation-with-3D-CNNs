1172ecae66fbea41f53ced72d7f5208d46ca9cf7
X <moon0shang@gmail.com>
2018-10-23 15:49:58 +0800

    ok

--- TSDF.py
+++ TSDF.py
@@ -1,11 +1,4 @@
 import numpy as np
-import scipy.io as sio
-
-"""
-def set_m(resolution=32):
-
-    return resolution
- """
 
 
 def tsdf(hand_points, hand_ori, pic_info):
@@ -36,8 +29,6 @@ def tsdf(hand_points, hand_ori, pic_info):
     tsdf_v = tsdf_cal(depth_ori, pic_info, vox_ori,
                       voxel_len, truncation, point_mid)
 
-    sio.savemat('./tsdf/tsdf.mat', {'tsdf': tsdf_v})
-
     return tsdf_v
 
 # need to be rewrite
@@ -63,88 +54,6 @@ def max_min_point(hand_ori):
     point_min[2] = np.min(z)
 
     return point_max, point_min
-
-
-"""
-wrong method that takes too much time, don't use it!
-def max_min_point(blockdim, hand_ori, pic_info):
-
-    [fFocal_msra, img_height, img_width, bb_top, bb_bottom,
-        bb_left, bb_right, bb_width, bb_height] = pic_info
-
-    # mm_threadperblock = 128
-    center_x = 160
-    center_y = 120
-    point_max_min = np.empty([blockdim, 6], dtype=np.float32)
-
-    point_min = np.empty((128, 3), dtype=np.float32)
-    point_max = np.empty((128, 3), dtype=np.float32)
-    # '''此处需要一个循环'''
-    for bid in range(blockdim):
-        for tid in range(128):
-            # cuda.grid = threadidx.x+blockidx.x*blockdim.x
-            # threadidx.x =0~127; blockidx.x=0~blockdim-1
-            # blockdim.x=128
-            pos = tid+bid*128
-            x = pos % bb_width + bb_left
-            y = pos / bb_width + bb_top
-            cam_z = hand_ori[pos]
-            if abs(cam_z) < 1:
-                point_min[tid][0], point_min[tid][1], point_min[tid][2] = 9999, 9999, 9999
-                point_max[tid][0], point_max[tid][1], point_max[tid][2] = - \
-                    9999, -9999, -9999
-            else:
-                coeff = cam_z / fFocal_msra
-                cam_x = coeff * (x - center_x)
-                cam_y = coeff * (y - center_y)
-                point_min[tid][0], point_min[tid][1], point_min[tid][2] = cam_x, cam_y, cam_z
-                point_max[tid][0], point_max[tid][1], point_max[tid][2] = cam_x, cam_y, cam_z
-
-            s = 1
-            while s < 128:
-                index = 2 * s * tid
-                if index + s < 128:
-                    point_min[index][0] = min(
-                        point_min[index + s][0], point_min[index][0])
-                    point_min[index][1] = min(
-                        point_min[index + s][1], point_min[index][1])
-                    point_min[index][2] = min(
-                        point_min[index + s][2], point_min[index][2])
-                    point_max[index][0] = min(
-                        point_max[index + s][0], point_max[index][0])
-                    point_max[index][1] = min(
-                        point_max[index + s][1], point_max[index][1])
-                    point_max[index][2] = min(
-                        point_max[index+s][2], point_max[index][2])
-
-                s *= 2
-
-            if tid == 0:
-                point_max_min[bid][0] = point_min[0][0]
-                point_max_min[bid][1] = point_min[0][1]
-                point_max_min[bid][2] = point_min[0][2]
-                point_max_min[bid][3] = point_max[0][0]
-                point_max_min[bid][4] = point_max[0][1]
-                point_max_min[bid][5] = point_max[0][2]
-
-    return point_max_min
-
- """
-"""
-    x = hand_points[:, 0]
-    y = hand_points[:, 1]
-    z = hand_points[:, 2]
-
-    x_max = np.max(x)
-    x_min = np.min(x)
-    y_max = np.max(y)
-    y_min = np.min(y)
-    z_max = np.max(z)
-    z_min = np.min(z)
-    point_max_min = np.array([x_min, y_min, z_min, x_max, y_max, z_max])
-
-    return point_max_min
- """
 
 
 def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation, point_mid):
@@ -224,60 +133,3 @@ def tsdf_cal(depth_ori, pic_info, vox_ori, voxel_len, truncation, point_mid):
                 tsdf_v[2, z, y, x] = tsdf_z
 
     return tsdf_v
-
-
-""" 
-def z_TSDF(hand_points,pic_info):
-
-    M = 32
-    fFocal = 242.1
-    voxel_center = [160, 120, 0]
-
-    x = hand_points[:, 0]
-    y = hand_points[:, 1]
-    z = hand_points[:, 2]
-
-    point_max = np.array([np.max(x), np.max(y), np.max(z)], dtype=np.float32)
-    point_min = np.array([np.min(x), np.min(y), np.min(z)], dtype=np.float32)
-    point_mid = (point_min + point_max) / 2
-    len_e = point_max - point_min
-    max_l = np.max(len_e)
-    voxel_len = max_l / M
-    truncation = voxel_len * 3
-    vox_ori = point_mid-max_l/2+voxel_len/2
-    tsdf_v = np.ones((M, M, M))
-
-    signed_distance = np.linalg.norm(hand_points - voxel_center, axis=1)
-
-    for zi in range(32):
-        for yi in range(32):
-            for xi in range(32):
-                vox_center = vox_ori + \
-                    np.array([xi * voxel_len, yi * voxel_len, zi * voxel_len])
-                vox_depth = -vox_center[2]
-                q = fFocal / vox_depth
-                pix_x = int(round(vox_center[0] * q + 160))
-                pix_y = int(round(-vox_center[1] * q + 120))
-                if pix_x < l or pix_x >= r or pix_y < t or pix_y >= b:
-                    continue
-                idx = (pix_y-t)*b_w+pix_x-l
-                # print vox_center,x,y,z,pix_x,pix_y,idx
-                pix_depth = s['data'][idx]
-                if pix_depth == 0:
-                    continue
-                diff = (pix_depth - vox_depth)/truncation
-                if diff >= 1 or diff <= -1:
-                    continue
-                tsdf_v[z, y, x] = diff
-
-    return tsdf_v
-"""
-
-"""     l_x = np.max(x) - np.min(x)
-    l_y = np.max(y) - np.min(y)
-    l_z = np.max(z) - np.min(z)
-
-    l_voxel = max(l_x, l_y, l_z) / M
-    truncation = 3*l_voxel
-
-    volume = np.zeros((M, M, M)) """
