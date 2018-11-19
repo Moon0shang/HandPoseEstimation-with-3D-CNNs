87ebec508bc6ec5c96e22014b4050087357a96e4
X <moon0shang@gmail.com>
2018-11-19 23:04:05 +0800

    data process done for now!

--- 3D_CNN/dataset.py
+++ 3D_CNN/dataset.py
@@ -9,7 +9,7 @@ import scipy.io as sio
 import torch.utils.data as data
 
 
-class TsdfDataset(data.dataset):
+class MSRA_Dataset(data.dataset):
     def __init__(self, root_path, opt, train=True):
         self.root_path = root_path
         self.train = train
@@ -24,54 +24,71 @@ class TsdfDataset(data.dataset):
             self.SUBJECTS = 3
             self.GESTURES = 2
 
-        # self.total_frame_num = self.__total_frame_num()  # 从Volume Length中获取的长度
+        self.total_num = self.__getlength(self.root_path)
 
         # 初始化 python 数组
-        self.tsdf = np.empty()
-        self.ground_truth = np.empty()
-        self.max_l = np.empty()
-        self.mid_p = np.empty()
+        self.tsdf = np.empty([self.total_num, 3, 32, 32, 32], dtype=np.float32)
+        self.ground_truth = np.empty([self.total_num, 63])
+        self.max_l = np.empty(self.total_num)
+        self.mid_p = np.empty([self.total_num, 3])
 
         self.start_index = 0
         self.end_index = 0
         # 获取训练数据路径并加载数据
+        results = sorted(os.listdir(self.root_path))[:9]
         if self.train:
             for sub in range(self.SUBJECTS):
                 if sub != self.test_idx:
-                    for ges in range(self.GESTURES):
-                        data_dir = os.path.join(self.root_path,)
-                        print("Training: " + data_dir)
-                        self.__loaddata(data_dir)
+                    data_dir = os.path.join(self.root_path, results[sub])
+                    print("Training: " + data_dir)
+                    self.__loaddata(data_dir)
         else:
-            for ges in range(self.GESTURES):
-                data_dir = os.path.join(self.root_path,)
-                print("Testing: " + data_dir)
-                self.__loaddata(data_dir)
+            data_dir = os.path.join(self.root_path, results[self.test_idx])
+            print("Testing: " + data_dir)
+            self.__loaddata(data_dir)
 
         self.tsdf = torch.from_numpy(self.tsdf)
         self.ground_truth = torch.from_numpy(self.ground_truth)
 
     def __getitem__(self, index):
-        """still have problems"""
-        return self.tsdf[index, :, :, :, :], self.ground_truth[index, :, :], self.max_l[index], slef.mid_p[index, :]
+        """return index data"""
+        return self.tsdf[index, :, :, :, :], self.ground_truth[index, :], self.max_l[index], self.mid_p[index, :]
 
     def __len__(self):
         """still have problems"""
         return self.tsdf.size(0)
 
     def __loaddata(self, data_dir):
-        data = sio.loadmat(os.path.join(data_dir, 'tsdf.mat'))
-        tsdf = data['tsdf']
-        ground_truth = sio.loadmat(os.path.join(data_dir, "ground truth.mat"))
-        self.start_index = self.end_index + 1
-        self.end_index = self.end_index + len(tsdf)
+        "load data from preprocess files"
+        files = sorted(os.listdir(data_dir))
 
-        self.tsdf[(self.start_index - 1):self.end_index, :, :, :, :] = tsdf.astype(
-            np.float32)
-        self.ground_truth[(self.start_index - 1):self.end_index,
-                          :, :] = ground_truth['ground_truth'].astype(np.float32)
-        self.max_l[(self.start_index - 1):self.end_index] = data['max_l'].astype(np.float32)
-        self.mid_p[(self.start_index - 1):self.end_index,
-                   :] = data['mid_p'].astype(np.float32)
+        for ges in self.GESTURES:
+            data = sio.loadmat(os.path.join(data_dir, files[ges+17]))
+            tsdf = data['tsdf']
+            ground_truth = sio.loadmat(os.path.join(data_dir, files[ges]))
+            self.start_index = self.end_index
+            self.end_index = self.end_index + tsdf.shape[0]
+
+            self.tsdf[self.start_index:self.end_index,
+                      :, :, :, :] = tsdf.astype(np.float32)
+            self.ground_truth[self.start_index:self.end_index,
+                              :] = ground_truth['ground_truth'].astype(np.float32)
+            self.max_l[self.start_index:self.end_index] = data['max_l'].astype(
+                np.float32)
+            self.mid_p[self.start_index:self.end_index,
+                       :] = data['mid_p'].astype(np.float32)
 
     def __getlength(self, data_dir):
+        "need modify"
+        files = sorted(os.listdir(data_dir))[9:]
+
+        if self.train:
+            total_num = 0
+            for idx, name in enumerate(files):
+                if idx != self.test_idx:
+                    num = sio.loadmat(os.path.join(data_dir, name))
+                    total_num += num['num']
+        else:
+            total_num = sio.loadmat(l_dir)
+
+        return total_num
--- 3D_CNN/eval-old.py
+++ 3D_CNN/eval-old.py
@@ -47,17 +47,6 @@ parser.add_argument('--JOINT_NUM', type=int, default=21,
                     help='number of joints')
 parser.add_argument('--INPUT_FEATURE_NUM', type=int,
                     default=6,  help='number of input point features')
-parser.add_argument('--PCA_SZ', type=int, default=42,
-                    help='number of PCA components')
-parser.add_argument('--knn_K', type=int, default=64,  help='K for knn search')
-parser.add_argument('--sample_num_level1', type=int,
-                    default=512,  help='number of first layer groups')
-parser.add_argument('--sample_num_level2', type=int,
-                    default=128,  help='number of second layer groups')
-parser.add_argument('--ball_radius', type=float, default=0.015,
-                    help='square of radius for ball query in level 1')
-parser.add_argument('--ball_radius2', type=float, default=0.04,
-                    help='square of radius for ball query in level 2')
 
 parser.add_argument('--test_index', type=int, default=0,
                     help='test index for cross validation, range: 0~8')
